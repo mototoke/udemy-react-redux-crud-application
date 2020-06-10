f0f32a6c09a400de41b67863fc3657cd98619ae1
mototoke <mototoke@hogehoge.com>
2020-06-11 01:10:53 +0900

    implement action and index.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React from 'react';
 
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "Noname" },
-  ]
+const App = () => (<Counter />)
 
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  )
-}
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
-}
+  handlPlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
 
-User.defaultProps = {
-  age: 1
+  handlMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlPlusButton}>+1</button>
+        <button onClick={this.handlMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
